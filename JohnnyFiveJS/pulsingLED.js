9646eb69232a1d6158c67de08ef4c9298426df31
AlminCode <almin.islamovic@gmail.com>
2018-05-22 23:56:53 +0200

    Added folders for ino and js files

---
+++ JohnnyFiveJS/pulsingLED.js
@@ -0,0 +1,29 @@
+var five = require("johnny-five");
+// var board = new five.Board();
+
+var board = new five.Board({
+  port: "COM3"
+});
+
+board.on("ready", function() {
+  var led = new five.Led(11);
+
+  led.pulse({
+    easing: "linear",
+    duration: 3000,
+    cuePoints: [0, 0.2, 0.4, 0.6, 0.8, 1],
+    keyFrames: [0, 10, 0, 50, 0, 255],
+    onstop: function() {
+      console.log("Animation stopped");
+    }
+  });
+
+  // Stop and turn off the led pulse loop after
+  // 12 seconds (shown in ms)
+  this.wait(12000, function() {
+    // stop() terminates the interval
+    // off() shuts the led off
+    led.stop().off();
+  });
+
+});
